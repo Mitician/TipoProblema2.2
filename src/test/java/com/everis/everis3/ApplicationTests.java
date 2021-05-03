85a685faa4e3424f7f85001f5a0aecdb922bc9ee
Cristofer <ceql@live.cl>
2021-05-03 15:19:48 -0400

    version 1

---
+++ src/test/java/com/everis/everis3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.everis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
